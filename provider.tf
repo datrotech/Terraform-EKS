68e2c1ffcd46565d2e98d14270f99fcd1b8bd7ab
datrotech <datrotechnology@gmail.com>
2025-08-08 07:51:13 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ provider "aws" {
 # configure backend
 terraform {
   backend "s3" {
-    bucket         = "devops-shack-s3"
+    bucket         = "Bob"
     key            = "eks.terraform.tfstate"
     region         = "us-west-1"
     
